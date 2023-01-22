1f898775e4627c81a0d3babe19aba28317c953ad
5-30 <97797879+5-30@users.noreply.github.com>
2023-01-22 09:46:57 +0100

    Removed useless code
    User-Agent is not necessary here.

--- main.py
+++ main.py
@@ -3,10 +3,7 @@ import json
 
 link = input("Which link should I extract tags from ? ")
 
-headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0"
-    }
-req = requests.get(f'{link}.json', headers=headers)
+req = requests.get(f'{link}.json')
 
 jsonArray = json.loads(req.text)
 
